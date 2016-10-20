c0dc6d76339d9b1583578e611bbc3551ac4b1818
admin <admin>
2016-10-20 10:28:25 +0200

    Initial commit

---
+++ src/main/java/com/comquent/RestcurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.comquent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcurrencyApplication.class, args);
+	}
+}
